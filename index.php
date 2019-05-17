9590c06f57ed2e45a695edf06536459c82ccc37d
jcmunoz <jcmunoz@smdigital.com.co>
2019-05-17 17:52:50 -0500

    .

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
-<h1>This is my index</h1>
+  <?php
+    if(have_posts()): while (have_posts()): the_post();?>
+
+      <h3><?php the_title(); ?></h3>
+      <small>Plublicado el <?php the_time('F j, Y'); ?> a las <?php the_time('g:i a'); ?>, en <?php the_category(); ?></small>
+      <p><?php the_content(); ?></p>
+      <hr>
+
+    <?php endwhile; ?>
+  <?php endif; ?>
 <?php get_footer(); ?>
